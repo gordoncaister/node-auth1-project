c42682756e580f579dda9e7b23c374c519c6bcf5
Gordon Caister <gordoncaister@gmail.com>
2020-05-18 15:34:52 -0500

    set up router. get '/' working on postman

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require("../users/users-router.js");
-const authRouter = require("../auth/router.js");
+
 const server = express();
 
 server.use(helmet());
@@ -10,10 +10,9 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/users", usersRouter);
-server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
-  res.json({ api: "up" });
+  res.json({ message: "how did you get here?" });
 });
 
 module.exports = server;
---
+++ users/users-router.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+const Users = require("./users-model.js");
+
+
+module.exports = router;
